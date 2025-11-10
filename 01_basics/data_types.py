72b30b2d474a87cc91e3582f690bba5fee6ca924
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-11-11 02:11:07 +0800

    feat: python data types

---
+++ 01_basics/data_types.py
@@ -0,0 +1,66 @@
+"""Data types in Python."""
+
+# Text Type
+x = "This is a statement." # str
+
+# Numeric Types 
+x = 10         #int
+x = 20.5       #float
+x = 1j         #complex
+
+# Sequence Types
+x = ["Apple", "Orange", "Banana"]  #list
+x = ("Apple", "Orange", "Banana")  #tuple
+x = range(5)   #range
+
+# Mapping Type
+x = {"name": "Alice", "age": 25}   #dict
+
+# Set Types
+x = {"Apple", "Orange", "Banana"}  #set
+x = frozenset({"Apple", "Orange", "Banana"}) #frozenset
+
+# Boolean Type
+x = True       #bool
+
+# Binary Types
+x = b"Hello"   #bytes
+x = bytearray(5)         #bytearray
+x = memoryview(bytes(5)) #memoryview
+
+# None Type
+x = None       #NoneType
+
+"""Setting specific data types in Python using constructor functions."""
+
+# Text Type
+x = str("This is a statement.")
+
+# Numeric Types 
+x = int(10)         #int
+x = float(20.5)     #float
+x = complex(1j)     #complex
+
+# Sequence Types
+x = list(("Apple", "Orange", "Banana"))    #list
+x = tuple(("Apple", "Orange", "Banana"))   #tuple
+x = range(5)   #range
+
+# Mapping Type
+x = dict(name="John", age=30)  #dict
+
+# Set Types
+x = set(("Apple", "Orange", "Banana"))  #set
+x = frozenset(("Apple", "Orange", "Banana")) #frozenset
+
+# Boolean Type
+x = bool(1)       #bool
+
+# Binary Types
+x = bytes(5)   #bytes
+x = bytearray(5)         #bytearray
+x = memoryview(bytes(5)) #memoryview
+
+
+
+
