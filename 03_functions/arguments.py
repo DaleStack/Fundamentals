6a64208fb7314da5cd8a3ac2b0fdc3afcb767bdb
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-11-18 23:47:02 +0800

    feat: enhance arguments section with data type examples and positional-only arguments

--- 03_functions/arguments.py
+++ 03_functions/arguments.py
@@ -42,3 +42,53 @@ describe_person(age=30, city="New York", name="Bob")  # Arguments passed by keyw
 Positional arguments are assigned based on their position in the function call, while keyword arguments are assigned 
 by parameter name."""
 
+
+
+"""Passsing different data types as arguments"""
+
+def display_info(person):
+    """Display information about a person."""
+    print(f"Name: {person['name']}, Age: {person['age']}, City: {person['city']}")
+
+person_info = {'name': 'Charlie', 'age': 28, 'city': 'Los Angeles'}
+display_info(person_info)  # Passing a dictionary as an argument
+
+
+def list_fruits(fruits):
+    """Display a list of fruits."""
+    for fruit in fruits:
+        print(fruit)
+
+fruit_list = ['Apple', 'Banana', 'Cherry']
+list_fruits(fruit_list)  # Passing a list as an argument
+
+
+"""Return value"""
+
+def multiply(x, y):
+    """Multiply two numbers and return the result."""
+    return x * y
+
+result = multiply(6, 7)
+print(f"The result of multiplication is: {result}")
+
+"""Function can return any data type, including lists, dictionaries, or even other functions."""
+def create_person(name, age):
+    """Create a person dictionary and return it."""
+    return {'name': name, 'age': age}
+
+person = create_person("Diana", 25)
+print(person)  # Output: {'name': 'Diana', 'age': 25}
+
+
+"""Positional-Only and Keyword-Only Arguments:"""
+def get_info(name, /, age, *, city):
+    """Get information about a person."""
+    print(f"{name} is {age} years old and lives in {city}.")
+
+get_info("Eve", 22, city="Miami")  # Correct usage
+# before / name must be positional
+# after * city must be keyword
+# get_info(name="Eve", age=22, city="Miami")  # This would raise an error
+# get_info("Eve", age=22, "Miami")  # This would raise an error
+
